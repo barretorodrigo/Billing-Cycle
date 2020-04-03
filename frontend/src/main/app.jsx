f7bed02990c74fe469bb3335f2b360c826a915ff
barretorodrigo <barretorodrigo@gmail.com>
2020-04-03 14:54:43 -0300

    Frontend navigation

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import '../common/template/dependencies';
-import Header from '../common/template/header.jsx';
+import Header from '../common/template/header';
+import Sidebar from '../common/template/sidebar';
+import Footer from '../common/template/footer'
+import Routes from './routes';
 
 export default props =>(
     <div className="wrapper">
         <Header />
+        <Sidebar />
+        <div className="content-wrapper">
+            <Routes />
+        </div>
+        <Footer />
     </div>
 )
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,25 +1,25 @@
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
-    entry:'./src/index.jsx',
-    output:{
+    entry: './src/index.jsx',
+    output: {
         path: __dirname + '/public',
         filename: './app.js'
     },
-    devServer:{
+    devServer: {
         port: 8080,
         contentBase: './public',
     },
-    resolve:{
-        extensions: ['', '.js', ',jsx'],
-        alias:{
+    resolve: {
+        extensions: ['', '.js', '.jsx'],
+        alias: {
             modules: __dirname + '/node_modules',
             jquery: 'modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
-            bootstrap: 'modules/admin-lte/bootstrap/js/bootstrap.js',
+            bootstrap: 'modules/admin-lte/bootstrap/js/bootstrap.js'
         }
     },
-    plugins:[
+    plugins: [ 
         new webpack.ProvidePlugin({
             $: 'jquery',
             jQuery: 'jquery',
